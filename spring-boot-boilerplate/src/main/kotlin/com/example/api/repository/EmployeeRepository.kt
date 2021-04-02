a94b6603f4899ffd91ad54b1fb9157a93b825b2b
Anirban Chatterjee <canirban99@gmail.com>
2021-04-02 15:58:53 +0200

    Refactor code

--- spring-boot-boilerplate/src/main/kotlin/com/example/api/repository/EmployeeRepository.kt
+++ spring-boot-boilerplate/src/main/kotlin/com/example/api/repository/EmployeeRepository.kt
@@ -2,5 +2,7 @@ package com.example.api.repository
 
 import com.example.api.repository.model.Employee
 import org.springframework.data.jpa.repository.JpaRepository
+import org.springframework.stereotype.Repository
 
+@Repository
 interface EmployeeRepository : JpaRepository<Employee, Long>
